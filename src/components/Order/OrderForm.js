e9121e89026ed93cb4ab0c2a310cb3c93892adb5
TheValkDokk <91455763+TheValkDokk@users.noreply.github.com>
2021-10-02 00:32:11 +0700

    add components

---
+++ src/components/Order/OrderForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../../layouts/Form'
+
+
+export default function OrderForm() {
+    return (
+        <Form noValidate autoComplete="off">
+            <input />
+            <select />
+        </Form>
+    )
+}

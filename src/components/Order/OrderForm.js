3fa061cd0b8143cec22657dc9a836edb1dc8b239
TheValkDokk <91455763+TheValkDokk@users.noreply.github.com>
2021-10-03 10:40:11 +0700

    update files

--- src/components/Order/OrderForm.js
+++ src/components/Order/OrderForm.js
@@ -1,57 +1,110 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from '../../layouts/Form'
-import { Grid } from '@material-ui/core'
+import { ButtonGroup, Grid, InputAdornment, makeStyles, Button as MuiButton } from '@material-ui/core'
 import { Input, Select, Button } from '../../controls'
+import ReplayIcon from '@material-ui/icons/Replay';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+import ReorderIcon from '@material-ui/icons/Reorder';
+import { createAPIEndpoint, ENDPOINTS } from '../../api'
 
 const pMethods = [
-  { id: 'none', title: 'Select' },
-  { id: 'Cash', title: 'Cash' },
-  { id: 'Card', title: 'Card' }
+	{ id: 'none', title: 'Select' },
+	{ id: 'Cash', title: 'Cash' },
+	{ id: 'Card', title: 'Card' }
 ]
 
-export default function OrderForm(props) {
-  const { values, errors, handleInputChange } = props
+const useStyles = makeStyles(theme => ({
+	adornmentText: {
+		'& .MuiTypography-root': {
+			color: '#f3b33d',
+			fontWeight: 'bolder',
+			fontSize: '1.5em'
+		}
+	},
+	submitButtonGroup: {
+		backgroundColor: '#f3b33d',
+		color: '#000',
+		margin: theme.spacing(1),
+		'& .MuiButton-label': {
+			textTransform: 'none'
+		},
+		'&:hover': {
+			backgroundColor: '#f3b33d',
+		}
+	}
+}))
 
-  return (
-    <Form>
-      <Grid container>
-        <Grid item xs={6}>
-          <Input
-            disabled
-            label='Order Number'
-            name='orderNumber'
-            value={values.orderNumber}
-          />
-          <Select
-            label='Customer'
-            name='customerId'
-            value={values.customerId}
-            onChange={handleInputChange}
-            options={[
-              { id: 0, title: 'Select' },
-              { id: 1, title: 'Customer 1' },
-              { id: 2, title: 'Customer 2' },
-              { id: 3, title: 'Customer 3' },
-              { id: 4, title: 'Customer 4' }
-            ]}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Select
-            label='Payment Method'
-            onChange={handleInputChange}
-            name='pMethod'
-            options={pMethods}
-            value={values.pMethod}
-          />
-          <Input
-            disabled
-            label='Grand Total'
-            name='gTotal'
-            value={values.gTotal}
-          />
-        </Grid>
-      </Grid>
-    </Form>
-  )
+export default function OrderForm(props) {
+	const { values, errors, handleInputChange } = props
+	const classes = useStyles();
+
+	const [customerList, setCustomerList] = useState([])
+
+	useEffect(() => {
+		createAPIEndpoint(ENDPOINTS.CUSTOMER).fetchAll()
+			.then(res => {
+				let customerList = res.data.map(item => ({
+					id: item.customerId,
+					title: item.customerName
+				}))
+				customerList = [{ id: 0, title: 'Select' }].concat(customerList)
+				setCustomerList(customerList)
+			})
+			.catch(err => console.log(err))
+	}, [])
+
+	return (
+		<Form>
+			<Grid container>
+				<Grid item xs={6}>
+					<Input
+						disabled
+						label='Order Number'
+						name='orderNumber'
+						value={values.orderNumber}
+						InputProps={{
+							startAdornment: <InputAdornment
+								className={classes.adornmentText}
+								position="start">#</InputAdornment>
+						}}
+					/>
+					<Select
+						label='Customer'
+						name='customerId'
+						value={values.customerId}
+						onChange={handleInputChange}
+						options={customerList}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<Select
+						label='Payment Method'
+						onChange={handleInputChange}
+						name='pMethod'
+						options={pMethods}
+						value={values.pMethod}
+					/>
+					<Input
+						disabled
+						label='Grand Total'
+						name='gTotal'
+						value={values.gTotal}
+						InputProps={{
+							startAdornment: <InputAdornment
+								className={classes.adornmentText}
+								position="start">$</InputAdornment>
+						}}
+					/>
+					<ButtonGroup className={classes.submitButtonGroup}>
+						<MuiButton size="large" endIcon={<RestaurantIcon />} type="submit">Submit</MuiButton>
+						<MuiButton size="small" startIcon={<ReplayIcon />} />
+					</ButtonGroup>
+					<Button
+						size="large"
+						startIcon={<ReorderIcon />}
+					>Order</Button>
+				</Grid>
+			</Grid>
+		</Form>
+	)
 }
